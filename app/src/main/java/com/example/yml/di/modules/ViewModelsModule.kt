47a9eff0eaa0aaa164481478cd33058dd9762efd
antonfcss <antonfcss96@gmail.com>
2022-07-05 23:48:52 +0300

    add Recycler

--- app/src/main/java/com/example/yml/di/modules/ViewModelsModule.kt
+++ app/src/main/java/com/example/yml/di/modules/ViewModelsModule.kt
@@ -2,16 +2,18 @@ package com.example.yml.di.modules
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.example.yml.presentation.features.about.AboutMovieViewModel
 import com.example.yml.di.vm.DaggerViewModelFactory
 import com.example.yml.di.vm.ViewModelKey
+import com.example.yml.presentation.features.about.AboutMovieViewModel
 import com.example.yml.presentation.features.movie.MovieViewModel
 import com.example.yml.presentation.features.mytop.MyTopViewModel
 import com.example.yml.presentation.features.popular.PopularViewModel
+import com.example.yml.presentation.features.search.SearchViewModel
 import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
 import javax.inject.Singleton
+
 //Хранение ViewModel. Сдесь мы указываем всё ViewModel, когда создаём новые экраны
 @Module
 abstract class ViewModelsModule {
@@ -31,12 +33,18 @@ abstract class ViewModelsModule {
 
     @Binds
     @IntoMap
-    @ViewModelKey(MyTopViewModel::class)
-    abstract fun bindMyTopViewModel(MyTopViewModel: MyTopViewModel): ViewModel
+    @ViewModelKey(PopularViewModel::class)
+    abstract fun bindPopularViewModel(PopularViewModel: PopularViewModel): ViewModel
 
     @Binds
     @IntoMap
-    @ViewModelKey(PopularViewModel::class)
-    abstract fun bindPopularViewModel(PopularViewModel: PopularViewModel): ViewModel
+    @ViewModelKey(SearchViewModel::class)
+    abstract fun bindSearchViewModel(SearchViewModel:SearchViewModel): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(MyTopViewModel::class)
+    abstract fun bindMyTopViewModel(MyTopViewModel: MyTopViewModel): ViewModel
+
 
 }
--- app/src/main/java/com/example/yml/presentation/features/popular/PopularFragment.kt
+++ app/src/main/java/com/example/yml/presentation/features/popular/PopularFragment.kt
@@ -1,14 +1,33 @@
 package com.example.yml.presentation.features.popular
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.yml.App
 import com.example.yml.databinding.FragmentPopularBinding
 import com.example.yml.presentation.base.BaseFragment
 import com.example.yml.presentation.base.getViewModelFromFactory
+import com.example.yml.presentation.features.popular.recycler.MoviesAdapter
 
-class PopularFragment: BaseFragment<PopularViewModel, FragmentPopularBinding>() {
+class PopularFragment : BaseFragment<PopularViewModel, FragmentPopularBinding>() {
+
+    private lateinit var adapter: MoviesAdapter
+
+
     override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentPopularBinding
         get() = FragmentPopularBinding::inflate
 
     override fun provideViewModel(): PopularViewModel = getViewModelFromFactory()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        adapter = MoviesAdapter()
+
+        val layoutManager = LinearLayoutManager(requireContext())
+        binding.recyclerview.layoutManager = layoutManager
+        binding.recyclerview.adapter = adapter
+    }
+
 }
